3057c97dfff892f0ad6caa83ab12e9e3db3c3ddc
meshachodoom <meshkobby98@gmail.com>
2023-02-16 06:21:53 -0800

    betty-documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/*This is the main function */
+/**
+*main - This is the main function description
+*
+*Return: 0 for successful exit
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+*main-This is the main function description
+*
+*Return: 0 for succesful exit
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+

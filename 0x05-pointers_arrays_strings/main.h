dcc67b9b78f9bfe881ccd7ce55c533172c4bbecd
meshachodoom <meshkobby98@gmail.com>
2023-02-27 06:32:29 -0800

    header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-#include <unistd.h>
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int_strlen(char *s);
@@ -16,6 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
 #endif /* MAIN_H */

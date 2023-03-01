d91c880027338affed705d5ea29d063fb28748ac
meshachodoom <meshkobby98@gmail.com>
2023-03-01 15:08:57 -0800

    header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
+int _strcmp(char * s1, char *s2);
 char *string_toupper(char *);
 char *leet(char *);
 char *rot13(char *);

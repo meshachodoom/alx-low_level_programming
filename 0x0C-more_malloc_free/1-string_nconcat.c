79a2fcdc5e66ead4556cf8f7a9f269a242a42188
meshachodoom <meshkobby98@gmail.com>
2023-03-16 14:20:58 -0700

    corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,44 +3,47 @@
 #include "main.h"
 
 /**
-* string_nconcat - concatenates 2 strings into a new string,
-* taking the first n from string 2. If n>strlen(s2),
-* use whole string s2.
-* @s1: first string
-* @s2: second string
-* @n: number of characters to take from second string
-* Return: new string, or null on failure
+* *string_nconcat - function that concatenates two strings
+* @s1: input string 1
+* @s2: input string 2
+* @n: first n bytes
+* Return: char
 */
 
 char *string_noncat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1, len2, i;
-char *ptr, *ret;
+char *str;
+char *nul = "";
+unsigned int x, y;
 
-if (!s1)
-s1 = "";
-if (!s2)
-s2 = "";
-
-for (len1 = 0, ptr = s1; *ptr; ptr++)
-len1++;
-for (len2 = 0, ptr = s2; ptr++)
-len2++;
-if (n > len2)
-n = len2;
-
-ret = malloc((len1 + n + 1) * sizeof(char));
-if (!ret)
-return (0);
-ptr = ret;
-while (*s1)
-*ptr++ = *s1++;
-i = 0;
-while (i < n)
+x = y = 0;
+if (s1 == NULL)
+s1 = nul;
+if (s2 == NULL)
+s2 = nul;
+while (s1[x] != '\0')
+x += 1;
+while (s2[y] != '\0')
+y += 1;
+y++;
+if (n >= y)
+n = y;
+str = malloc(sizeof(*str) * n + (x + 1));
+if (str == NULL)
+return (NULL);
+x = 0;
+while (s1[x] != '\0')
 {
-*ptr++ = s2[i++];
+str[x] = s1[x];
+x += 1;
 }
-*ptr = 0;
-
-return (ret);
+y = 0;
+while (s2[y] != '\0' && y < n)
+{
+str[x] = s2[y];
+x++;
+y++;
+}
+str[x] = '\0';
+return (str);
 }

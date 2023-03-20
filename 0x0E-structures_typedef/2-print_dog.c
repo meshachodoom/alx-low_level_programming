8df5f3416475fe8ab519e234f59074a70e118d84
meshachodoom <meshkobby98@gmail.com>
2023-03-20 11:09:04 -0700

    updates

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,20 +1,19 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 /**
-* print_dog - prints dog stats
-* @d: dog to print
+* print_dog - prints a struct dog
+* @d: dog structure
 */
 void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
 if (d->name == NULL)
-d->name = "(nil)";
+printf("Name: (nil)\n";
 if (d->age <= 0)
-printf("Age: (nil)\n");
-if (d->owner == NULL)
-d->owner = "(nil)";
-
+else
 printf("Name: %s\n", d->name);
-printf("Age: %.6f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)\n");
+else
 printf("Owner: %s\n", d->owner);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOGH
-#define DOGH
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * struct dog - a dog
@@ -12,8 +12,7 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-} dog;
-typedef dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

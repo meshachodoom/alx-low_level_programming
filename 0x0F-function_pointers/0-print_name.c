0ea1083fd274e4cf6078600c79a07e4af35dc7e4
meshachodoom <meshkobby98@gmail.com>
2023-03-23 13:44:03 -0700

    corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
 * print_name -function that prints name
-* @name:string that contains name
-* @f: pointer to function that prints name
+* @name: a name to be printed
+* @f: a function pointer to a callback function
+* Return: void
 */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }
